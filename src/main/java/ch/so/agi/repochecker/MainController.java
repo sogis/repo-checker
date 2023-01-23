8de673a9137f6aeca58f924dd9128238d91ad6df
edigonzales <edi.gonzales@gmail.com>
2023-01-23 19:15:15 +0100

    start refactoring

--- src/main/java/ch/so/agi/repochecker/MainController.java
+++ src/main/java/ch/so/agi/repochecker/MainController.java
@@ -1,178 +1,21 @@
 package ch.so.agi.repochecker;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.InputStreamResource;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.util.FileSystemUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import com.fasterxml.jackson.dataformat.xml.XmlMapper;
-
-import ch.so.agi.repochecker.model.Check;
-import ch.so.agi.repochecker.model.IliRepo;
-import ch.so.agi.repochecker.model.IliRepos;
-import ch.so.agi.repochecker.model.Result;
-
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.attribute.FileTime;
-import java.time.Instant;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
     private final Logger log = LoggerFactory.getLogger(this.getClass());
 
-    @Autowired
-    private CheckerService checker;
-        
-    @Autowired
-    private IliRepoRepository iliRepoRepository;
-    
-    @Autowired
-    private XmlMapper xmlMapper;
-    
-    @Value("${app.workDirectory}")
-    private String workDirectory;
-
-    @Value("${app.workDirectoryPrefix}")
-    private String workDirectoryPrefix;
-        
     @GetMapping("/ping")
     public ResponseEntity<String> ping()  {
-        return new ResponseEntity<String>("repo-checker", HttpStatus.OK);
+        return new ResponseEntity<String>("interlis-repo-checker", HttpStatus.OK);
     }
     
-    @GetMapping(value="/repositories", produces=MediaType.APPLICATION_XML_VALUE)
-    public ResponseEntity<String> repositories() throws IOException  {  
-        // TODO: Das verändert die Werte in den Originaldaten. 
-        // Die Methode sollte vielleicht besser eine Kopie erstellen und
-        // in dieser die Werte verändern.
-        var iliRepoList = updateLogFileLocation(iliRepoRepository.findAll());
-        var iliRepos = new IliRepos();
-        iliRepos.setIliRepos(iliRepoList);
-        var xmlString = xmlMapper.writeValueAsString(iliRepos);
-        return new ResponseEntity<String>(xmlString, HttpStatus.OK);
-    }
-    
-    @GetMapping("/results/{key}/{filename}") 
-    public ResponseEntity<?> getLog(@PathVariable String key, @PathVariable String filename) {        
-        var mediaType = new MediaType("text", "plain", StandardCharsets.UTF_8);
+    // Modellieren visuell mit XML beginnen. Hilft eventuell.
 
-        try {
-            var logFile = Paths.get(workDirectory, key, filename).toFile();
-            var is = new FileInputStream(logFile);
-
-            return ResponseEntity.ok().header("Content-Type", "charset=utf-8")
-                    .contentLength(logFile.length())
-                    .contentType(mediaType)
-                    .body(new InputStreamResource(is));
-
-        } catch (FileNotFoundException e) {
-            throw new IllegalStateException(e);  
-        }
-    }
-    
-    @GetMapping("/")
-    public String show(Model model) {
-        var iliRepoList = updateLogFileLocation(iliRepoRepository.findAll());
-        Collections.sort(iliRepoList, new Comparator<IliRepo>() {
-            @Override
-            public int compare(IliRepo o1, IliRepo o2) {
-                var string0 = o1.getIdent().toLowerCase();
-                var string1 = o2.getIdent().toLowerCase();
-                if (string0.contains("https")) {
-                    string0 = string0.substring(8);
-                } else {
-                    string0 = string0.substring(7);
-                }
-                if (string1.contains("https")) {
-                    string1 = string1.substring(8);
-                } else {
-                    string1 = string1.substring(7);
-                }
-                return string0.compareTo(string1);
-            }
-        });
-        
-        model.addAttribute("repositories", iliRepoList);
-        return "gui";
-    } 
-
-    @Scheduled(cron="${app.checkCronExpression}")
-    //@Scheduled(cron="0 */4 * * * *")
-    private void checkRepos() {
-        try {
-            log.info("check repos...");
-            checker.checkRepos();
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw new IllegalStateException(e.getMessage());
-        } 
-    }
-    
-    @Scheduled(cron="0 0/2 * * * *")
-    private void cleanUp() {    
-        log.debug("cleaner...");
-        java.io.File[] tmpDirs = new java.io.File(workDirectory).listFiles();
-        if(tmpDirs!=null) {
-            for (java.io.File tmpDir : tmpDirs) {
-                if (tmpDir.getName().startsWith(workDirectoryPrefix)) {
-                    try {
-                        FileTime creationTime = (FileTime) Files.getAttribute(Paths.get(tmpDir.getAbsolutePath()), "creationTime");                    
-                        Instant now = Instant.now();
-                        
-                        long fileAge = now.getEpochSecond() - creationTime.toInstant().getEpochSecond();
-                        if (fileAge > 60*60*4) {
-                            log.info("deleting {}", tmpDir.getAbsolutePath());
-                            FileSystemUtils.deleteRecursively(tmpDir);
-                        }
-                    } catch (IOException e) {
-                        throw new IllegalStateException(e);
-                    }
-                }
-            }
-        }
-    }
-    
-    private List<IliRepo> updateLogFileLocation(List<IliRepo> iliRepoList) {
-        for (IliRepo iliRepo : iliRepoList) {
-            for (Map.Entry<Check, Result> entry : iliRepo.getResults().entrySet()) {
-                String logFile = entry.getValue().getLogFile();
-                entry.getValue().setLogFile(getLogFileLocation(logFile));
-            }
-        }
-        return iliRepoList;
-    }
-    
-    private String getLogFileLocation(String logFileName) {
-        String LOG_ENDPOINT = "results";
-         return fixUrl(getHost() + "/" + LOG_ENDPOINT + "/" + Utils.getLogFileUrlPathElement(logFileName));
-    }
-
-    private String fixUrl(String url) {
-        return url.replaceAll("(?<=[^:\\s])(\\/+\\/)", "/");
-    }
-
-    private String getHost() {
-        return ServletUriComponentsBuilder.fromCurrentContextPath().build().toUriString();
-    }
 }
--- src/main/java/ch/so/agi/repochecker/model/Check.java
+++
@@ -1,5 +0,0 @@
-package ch.so.agi.repochecker.model;
-
-public enum Check {
-    ILISITE_XML, ILIMODELS_XML, MODELS
-}
